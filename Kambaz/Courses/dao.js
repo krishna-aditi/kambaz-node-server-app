84e4cc1c749fa5e8129c5a5d94daec85a398efe8
Aditi Krishna <aditikrishna@Aditis-MacBook-Air.local>
2025-03-25 23:52:35 -0400

    fixed courses routes

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,15 +1,8 @@
 import Database from "../Database/index.js";
-
+import { v4 as uuidv4 } from "uuid";
 export function findAllCourses() {
     return Database.courses;
 }
-
-// export function findCoursesForEnrolledUser(userId) {
-//     const { courses, enrollments } = Database;
-//     const enrolledCourses = courses.filter((course) =>
-//         enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
-//     return enrolledCourses;
-// }
   
 export function findCoursesForEnrolledUser(userId) {
     const { courses, enrollments } = Database;
@@ -24,4 +17,20 @@ export function createCourse(course) {
     Database.courses = [...Database.courses, newCourse];
     return newCourse;
 }
+
+export function deleteCourse(courseId) {
+    const { courses, enrollments } = Database;
+    Database.courses = courses.filter((course) => course._id !== courseId);
+    Database.enrollments = enrollments.filter(
+      (enrollment) => enrollment.course !== courseId);
+}
+
+export function updateCourse(courseId, courseUpdates) {
+    const { courses } = Database;
+    const course = courses.find((course) => course._id === courseId);
+    Object.assign(course, courseUpdates);
+    return course;
+}
+  
+  
   
